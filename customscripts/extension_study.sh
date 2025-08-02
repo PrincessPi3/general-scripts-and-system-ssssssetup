237a03fb26841540004f6534d052d78c7b1f843f
PrincessPi3 <princesspi@proton.me>
2025-08-02 15:58:46 -0600

    stupidfuckinscriopp

--- customscripts/extension_study.sh
+++ customscripts/extension_study.sh
@@ -3,7 +3,7 @@ tmp_file="./exts.tmp"
 exts_study_file="./extension_study.txt"
 
 echo "" > "$tmp_file"
-find files -type f -exec bash -c "echo \"{}\" | awk -F. '{print \$NF}' | tee -a \"$tmp_file\"" \;
+find ./files -type f -exec bash -c "echo \"{}\" | awk -F. '{print \$NF}' | tee -a \"$tmp_file\"" \;
 cat "$tmp_file" | sort | uniq > "$exts_study_file"
 rm -f "$tmp_file"
 
