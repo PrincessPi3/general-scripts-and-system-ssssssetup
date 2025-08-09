8786bf3470fab95ab6a5e5d874b63545c298badb
PrincessPi3 <human.bagel@gmail.com>
2025-08-08 23:35:23 -0600

    1754717723

--- Windows-Scripts/force_me_off.ps1
+++ Windows-Scripts/force_me_off.ps1
@@ -9,26 +9,30 @@ param (
     $grace_minutes = 2
 )
 
+ $job = Start-Job -ScriptBlock { Get-Process } # default get-process selects self
+
 # some calcs
-$wait_minutes = (($Hours*60) + $Minutes)
+$wait_minutes = (($Hours*60)+$Minutes)
 $wait_seconds = ($wait_minutes*60)
 $total_wait_minutes = ($wait_minutes+$grace_minutes)
 $grace_seconds = ($grace_minutes*60)
 
-Write-Host "FORCING YOUR STUPID ASS OFF IN $Hours hours $Minutes minutes plus $total_wait_minutes minutes grace period`n"
+Write-Host "`nFORCING YOUR STUPID ASS OFF IN $Hours hours $Minutes minutes plus $grace_minutes minutes grace period`n"
 
 # current time
 Write-Host "$(Get-Date -Format 'hh:mm:ss tt') | Start Time"
 
 # shutdown time
-Write-Host "$((Get-Date).AddHours($Hours).AddMinutes($total_wait_minutes).ToString("hh:mm:ss tt")) | Forcing shutting down time ($Hours hours $Minutes minutes plus $grace_minutes minutes grace period)"
+Write-Host "$((Get-Date).AddHours($Hours).AddMinutes($total_wait_minutes).ToString("hh:mm:ss tt")) | Reboot Time"
 
 Write-Host "`nSleeping for $Hours hours $Minutes minutes...`n"
 
 # sleep
 Start-Sleep -Seconds ($wait_seconds)
 
-Write-Host "FORCING REBOOT IN $grace_minutes MINUTES"
+# delete the job
+Remove-Job -Id $job.Id
 
 # force reboot
+Write-Host "FORCING REBOOT IN $grace_minutes MINUTES"
 Restart-Computer -Force -Timeout $grace_seconds
