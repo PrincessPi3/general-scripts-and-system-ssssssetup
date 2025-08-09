2d6c2a73a5cd0dff0b58b470f8c2bdb06258cc75
PrincessPi3 <human.bagel@gmail.com>
2025-08-09 05:37:10 -0600

    1754739431

--- Windows-Scripts/force_me_off.ps1
+++ Windows-Scripts/force_me_off.ps1
@@ -10,12 +10,10 @@ param (
 $wait_minutes = (($Hours*60)+$Minutes)
 $wait_seconds = ($wait_minutes*60)
 $total_wait_minutes = ($wait_minutes+$grace_minutes)
+$total_wait_seconds = ($total_wait_minutes*60)
 $grace_seconds = ($grace_minutes*60)
 
 Write-Host "wait_seconds $wait_seconds wait_minutes $wait_minutes hours $Hours minutes $Minutes grace_seconds $grace_seconds grace_minutes $grace_minutes total_wait_minutes $total_wait_minutes"
-
-# schedule chkdsk to take up fuckin tons of time
-Start-Process -Verb RunAs -FilePath cmd.exe -ArgumentList  '/C "chkdsk /r C:"'
 
 Write-Host "`nFORCING YOUR STUPID ASS OFF IN $Hours hours $Minutes minutes plus $grace_minutes minutes grace period`n"
 
@@ -27,16 +25,22 @@ Write-Host "$((Get-Date).AddHours($Hours).AddMinutes($total_wait_minutes).ToStri
 
 Write-Host "`nSleeping for $Hours hours $Minutes minutes and forking to background to prevent cheating...`n"
 
-Start-Job -ScriptBlock {
-    # sleep
-    Start-Sleep -Seconds 3 # $wait_seconds
+function do_admin_shit {
+    # handle interactive shit right away
+    ## schedule chkdsk to take up fuckin tons of time
+    Start-Process -Verb RunAs -FilePath cmd.exe -ArgumentList  '/C "chkdsk /r C:"'
 
-    # force reboot
+    ## must use fuckin cmd bullshit grumble grumble
+    ## cancel with shutdown /a
+    shutdown /f /r /t $total_wait_seconds
+
+    # sleep
+    Start-Sleep -Seconds $total_wait_seconds
+
     ## popup
     $shell = New-Object -ComObject 'WScript.Shell'
     # $shell.Popup(string <MESSAGE>, int <MODE>, string <WINDOW_TITLE>, int <CONTROLS>)
     $shell.Popup("REBOOTING BY FORCE IN $grace_minutes MINUTES", 2, "REBOOTING AS FUCK IN $grace_minutes MINUTES", 0)    ## reboot, force, delay $grace_seconds seconds
-    ## must use fuckin cmd bullshit grumble grumble
-    ## cancel with shutdown /a
-    Start-Process -FilePath shutdown.exe -ArgumentList "/f /r /t $grace_seconds"
-} | Out-Null # no bizzle
+}
+
+do_admin_shit | Out-Null
