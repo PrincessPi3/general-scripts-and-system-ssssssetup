2e57a2f67ef40528bae4af2e4684456eedbbc36b
PrincessPi3 <human.bagel@gmail.com>
2025-11-30 17:17:51 -0700

    fixed fix_permissions abd added sync media

---
+++ Windows-Scripts/sync_media.ps1
@@ -0,0 +1,17 @@
+$media_viewer_dir="C:\Users\human\OneDrive\Documents\Git\Media-Viewer"
+Set-Location "$media_viewer_dir"
+
+Write-Host "Sillyfillyy synching media lolee"
+
+# get da synciedink
+gitsync
+gitsync
+
+# do da synchiedink
+wsl bash full_sync_normalization_wsl.sh bfy
+
+# do more sync at enddy to maek syre it goodywoo
+gitsync
+gitsync
+
+Write-Host "aahm done bein a sillyfilly fro noew"
