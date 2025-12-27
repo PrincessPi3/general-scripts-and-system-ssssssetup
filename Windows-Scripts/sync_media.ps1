6dcee72d30ba272aa21419e3494748b713ca8423
PrincessPi3 <human.bagel@gmail.com>
2025-12-27 09:14:22 -0700

    1766852063

--- Windows-Scripts/sync_media.ps1
+++ Windows-Scripts/sync_media.ps1
@@ -15,15 +15,6 @@ if($args[0] -eq "NUKE") {
 
 Write-Host "`nSillyfillyy synching media loleen`n"
 
-# Write-Host "`nChanging directory to $media_viewer_dir`n"
-# Set-Location "$media_viewer_dir"
-
-# get da synciedink
-# Write-Host "`nSyncing`n"
-# gitsync
-# Write-Host "`nSyncing Again`n"
-# gitsync
-
 # do da synchiedink
 Write-Host "`n`bPERFORMING THE DILDOSYNC`n`n"
 # wsl bash $media_viewer_dir_wsl/copy_local_wsl.sh
@@ -35,20 +26,6 @@ if($nuke) {
     wsl bash $media_viewer_dir_wsl/dildo_new_full_sync_total.sh
 }
 
-# Write-Host "`nDoing the syncy dink`n"
-# # wsl -d kali-wsl "\$(whoami) \$(uname -a)" # debug
-# if($nuke) {
-#     Write-Host "`nNUKING media loleen sync`n"
-#     wsl bash $media_viewer_dir_wsl/full_sync_normalization_wsl.sh byfn
-# } else {
-#     Write-Host "`nNormal media loleen sync`n"
-#     wsl bash $media_viewer_dir_wsl/full_sync_normalization_wsl.sh byf
-# }
-
-# normieize media
-# Write-Host "`nNormalizing favorites`n"
-# ssh pi3 "bash /var/www/html/Media-Viewer/normalize_favorites.sh"
-
 # remote esp-idf
 Write-Host "`nRunning esp-idf-tools update`n"
 if($nuke) {
@@ -58,14 +35,5 @@ if($nuke) {
     Write-Host "`nNormal esp-idf-tools update`n"
     ssh pi3 "bash /home/princesspi/esp/esp-idf-tools/esp-idf-tools-cmd.sh"
 }
-
-# reboot pi3 to apply updates
-# if($nuke) {
-#     Write-Host "`nNUKING reboot of pi3 to apply updates`n"
-#     ssh pi3 "sudo shutdown -r +10 'Rebooting in 10 minutes to apply updates'"
-# } else {
-#     Write-Host "`nSkipping remote reboot`n"
-# }
-
 
 Write-Host "`naahm done bein a sillyfilly fro noew`n"
