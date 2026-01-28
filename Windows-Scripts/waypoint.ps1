8fa6dc8cf53221da76dc1d5c800ed43c302f5adc
PrincessPi3 <human.bagel@gmail.com>
2026-01-28 02:59:51 -0700

    HII HII

--- Windows-Scripts/waypoint.ps1
+++ Windows-Scripts/waypoint.ps1
@@ -1,2 +1,9 @@
+if ($args[0]) { $commit_message=($args -join " ") } # if arg0 exists, join all passed args with spaces
+else { $commit_message="waypoint" } # or default to "waypoint"
+
+Write-Host "WAYPOINT!`n`tCommit Message: $commit_message`n"
+# add and commit
 git add .
-git commit -m "waypoint"
+git commit -m "$commit_message"
+
+Write-Host "Done :`"3"
